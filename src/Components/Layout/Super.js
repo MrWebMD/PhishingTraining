665c23be866bd8f30227bec1de01eb073b636b91
MrWebMD <345links@gmail.com>
2023-02-07 21:54:47 -0500

    Folder casing

--- src/Components/Layout/Super.js
+++
@@ -1,9 +0,0 @@
-const Super = (props) => {
-  return (
-    <header className="superHeader" data-aos="fade-in">
-      <div className="pageBoundary superHeader__contentWrapper">{props.children}</div>
-    </header>
-  );
-};
-
-export default Super;
--- src/Components/Typer.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-const Typer = () => {
-  const [text, setText] = useState("");
-};
-
-export default Typer;
